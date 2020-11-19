9c090c486e18d9a1103787e193681dc0b54d5863
stephenwalker2020 <helenzjoneso5298@gmai.com>
2020-11-19 11:47:48 +0800

    set network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 
+const {mnemonic, projectId, etherscanKey} = require("./secret.json")
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -18,5 +20,13 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url:  `https://ropsten.infura.io/v3/${projectId}`,
+      accounts: {
+        mnemonic: mnemonic,
+      }
+    }
+  }
 };
 
